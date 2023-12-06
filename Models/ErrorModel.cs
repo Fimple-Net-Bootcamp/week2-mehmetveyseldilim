e63a51506d708bde46db2fc644bed9b2d2b9cb29
mehmetveyseldilim <veyseldilim@gmail.com>
2023-12-06 18:51:18 +0300

    added global exception handler middleware

---
+++ Models/ErrorModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+
+namespace Weather.API.Models 
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+   
+}
--- Program.cs
+++ Program.cs
@@ -1,33 +1,46 @@
+using Microsoft.AspNetCore.Hosting;
 using Serilog;
+using Serilog.Extensions.Logging;
 using Weather.API.Extensions;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddAutoMapperService();
-builder.Services.AddEfSqLiteInMemoryDb();
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.ConfigureSerilogLogging(builder.Configuration);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program 
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAutoMapperService();
+        builder.Services.AddEfSqLiteInMemoryDb();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.ConfigureSerilogLogging(builder.Configuration);
+
+        var app = builder.Build();
+        var logger = app.Services.GetRequiredService<ILogger<Program>>(); // Use ILogger<Startup> here
+        app.ConfigureExceptionHandler(logger);
+
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseSerilogRequestLogging();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
 
-app.UseSerilogRequestLogging();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
